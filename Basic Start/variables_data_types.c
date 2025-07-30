78e8ec1f74ef7a06f2ec9aba34951209edc2247b
Sandeep Karmata <Sandeepkarmata1991@gmail.com>
2025-07-30 14:17:10 +0530

    Added Variables and Data Types Program in Basics
    "Every expert was once a beginner pressing 'Run' for the first time."

---
+++ Basic Start/variables_data_types.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int myAge = 18;            // Integer (whole number)
+    float myGPA = 8.5;         // Floating point number
+    char myGrade = 'A';        // Character
+
+    printf("I am %d years old.\n", myAge);
+    printf("My GPA is %.2f.\n", myGPA);
+    printf("My grade is %c.\n", myGrade);
+
+    return 0;
+}
